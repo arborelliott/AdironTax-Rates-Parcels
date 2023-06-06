f4c532695f98134fbdf2d40910a331bd85dbfdf8
arborelliott <Arborelliott@gmail.com>
2023-06-06 15:55:32 -0400

    added percentage of locality spending to script

--- Parcel_Tax_Merge_func.py
+++ Parcel_Tax_Merge_func.py
@@ -34,28 +34,27 @@ plt.rcParams['figure.dpi'] = 300
 
 #%% Importing files (tax, Parcel, Census)
 
-# TAX
-#tax_raw = pd.read_csv('Input/Real_Property_Tax_Rates_Levy_Data_By_Municipality__Beginning_2004.csv') 
+# TAX levy data
 tax_raw = pd.read_csv('Input/NYS_Tax_rates_Levy_Roll21.csv',dtype = {'FIPS_CODE':str,'STATE FIPS':str,'County FIPS':str,'Subdiv FIPS':str,'Place FIPS':str}) 
 tax = tax_raw
 
-# PARCEL
-#   Select Codes
+# PARCEL of selected codes
 parcel_raw = pd.read_csv('Input/Property_Assessment_Data_from_Local_Assessment_Rolls_931_980_940_932_990.csv',dtype = {'Print Key Code':str})
 
-#   All PROPERTIES AND CODES ***VERY SLOW***
-#       Only needed with first run
+# All PROPERTIES AND CODES ***VERY SLOW***  Only needed with first run
 #parcel_raw = pd.read_csv('Input/Property_Assessment_Data_from_Local_Assessment_Rolls.csv')
 #parcel_raw.to_pickle('Input/Property_Assessment_Data_from_Local_Assessment_Rolls.pkl')
-
 #parcel_raw = pd.read_pickle('Input/Property_Assessment_Data_from_Local_Assessment_Rolls.pkl')
 
-#
 parcel = parcel_raw
 
 # CENSUS
 county_census = pd.read_excel('Output/Census/County_Census.xlsx',dtype = {'county':str})
 subdiv_census = pd.read_excel('Output/Census/subdiv_Census.xlsx',dtype = {'county':str,'county subdivision':str})
+
+# LOCALITY BUDGETS
+county_budget = pd.read_excel('Input/Locality Spending/levelone21_county.xlsx')
+town_budget = pd.read_excel('Input/Locality Spending/levelone21_town.xlsx')
 
 
 #%% Cleaning
@@ -101,6 +100,9 @@ parcel = parcel[parcel['Roll Year'] == 2021]
 # Fix St lawrence naming
 tax['County'] = tax['County'].replace('St. Lawrence', 'St Lawrence')
 
+# Dropping not filed towns
+county_budget = county_budget[county_budget['Real Property Taxes and Assessments'] != 'Not Filed']
+town_budget = town_budget[town_budget['Real Property Taxes and Assessments'] != 'Not Filed']
 
 
 #%% Merging Data
@@ -150,16 +152,16 @@ merged_parcel_tax['School Rate'] = merged_parcel_tax['School District Tax Rate (
 #%% Calculating parcel tax cost
 
 # County
-merged_parcel_tax['County Tax Paid'] = merged_parcel_tax['County Rate'] * merged_parcel_tax['County Taxable Value'] /1000
+merged_parcel_tax['Parcel County Tax Paid'] = merged_parcel_tax['County Rate'] * merged_parcel_tax['County Taxable Value'] /1000
 
 # Municipal
-merged_parcel_tax['Municipal Tax Paid'] = merged_parcel_tax['Municipal Rate'] * merged_parcel_tax['Town Taxable Value'] /1000
+merged_parcel_tax['Parcel Municipal Tax Paid'] = merged_parcel_tax['Municipal Rate'] * merged_parcel_tax['Town Taxable Value'] /1000
 
 # School
-merged_parcel_tax['School Tax Paid'] = merged_parcel_tax['School Rate'] * merged_parcel_tax['School Taxable'] /1000
+merged_parcel_tax['Parcel School Tax Paid'] = merged_parcel_tax['School Rate'] * merged_parcel_tax['School Taxable'] /1000
 
 # Combined
-merged_parcel_tax['Combined Tax Paid'] = merged_parcel_tax['School Tax Paid'] + merged_parcel_tax['County Tax Paid'] + merged_parcel_tax['Municipal Tax Paid']
+merged_parcel_tax['Parcel Combined Tax Paid'] = merged_parcel_tax['Parcel School Tax Paid'] + merged_parcel_tax['Parcel County Tax Paid'] + merged_parcel_tax['Parcel Municipal Tax Paid']
 
 
 
@@ -202,27 +204,13 @@ all_parcel_tax = merged_parcel_tax
 
 def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     
-    
-    
-    ### Property Class
+    # Property Class subset
     classdisct = {931:'532a', 980:'Easments', 940:'Reforested_other',932:'532b',990:'Other'}
-
-    ###############
-    ###############
-
     taxcode = 'Allclass'
     
     if pclass != 'Allclass': # if pclass is not None, subset based on class code. 
         taxcode = classdisct[pclass]
         func_parcel_tax = func_parcel_tax[func_parcel_tax['Property Class'] == pclass]
-
-
-
-    # class 931 = tax 532a Taxable Forest Preserve
-    # class 980 = Taxable state-owned conservation easements
-    # class 940 = Reforested land and other related conservation purposes
-    # class 932 = tax 532b Other State-owned land under Section 532-b, c, d, e, f, or g
-    # class 990 = Other taxable state land assessments
    
     # Create File Path
     folder_path = f'Output/{taxcode}'
@@ -232,7 +220,7 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
         os.makedirs(folder_path)
     
     # County table
-    county_sum = func_parcel_tax.groupby(['County'])['County Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
+    county_sum = func_parcel_tax.groupby(['County'])['Parcel County Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
     county_sum['County FIPS'] = func_parcel_tax.groupby('County')['County FIPS'].first().reset_index(drop=True) # inclue county FIPS code
     county_sum['sum'] = county_sum['sum'].round(2)
     
@@ -248,7 +236,7 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     overall_total = overall_total.append(total_row)
     
     # Municipality table 
-    munic_sum = func_parcel_tax.groupby(['Municipality Name'])['Municipal Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
+    munic_sum = func_parcel_tax.groupby(['Municipality Name'])['Parcel Municipal Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
     munic_sum['Subdiv FIPS'] = func_parcel_tax.groupby('Municipality Name')['Subdiv FIPS'].first().reset_index(drop=True)
     munic_sum['sum'] = munic_sum['sum'].round(2)
     
@@ -263,7 +251,7 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     overall_total = overall_total.append(total_row)
     
     # School table
-    school_sum = func_parcel_tax.groupby(['School District Name'])['School Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
+    school_sum = func_parcel_tax.groupby(['School District Name'])['Parcel School Tax Paid'].agg(['sum', 'mean', 'count']).reset_index()
     school_sum['sum'] = school_sum['sum'].round(2)
     
     ## School table (total row)
@@ -284,16 +272,52 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     
     # Locality totals joined to main parcels list
     func_parcel_tax = func_parcel_tax.merge(county_sum_total[['sum','County FIPS']], on='County FIPS', how='left')
-    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total County Tax Received (k)'})
+    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total County Select Tax Received (k)'})
     
     func_parcel_tax = func_parcel_tax.merge(munic_sum_total[['sum','Subdiv FIPS']], on='Subdiv FIPS', how='left')
-    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total Municipal Tax Received (k)'})
+    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total Municipal Select Tax Received (k)'})
     
     func_parcel_tax = func_parcel_tax.merge(school_sum_total[['sum','School District Name']], on='School District Name', how='left')
-    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total School Tax Received (k)'})
+    func_parcel_tax = func_parcel_tax.rename(columns={'sum': 'Total School Select Tax Received (k)'})
                                                                          
+    # Joining locality budgets to parcels
+    ## County Budget to County Summary table
+    county_sum_total = county_sum_total.merge(county_budget[['County','Real Property Taxes and Assessments',
+                                                             'Local Revenues','Total Revenues']],
+                                              on = 'County', how = 'left')
+    ### Converting to (k)
+    county_sum_total['Real Property Taxes and Assessments (k)'] = county_sum_total['Real Property Taxes and Assessments'] /1000
+    county_sum_total['Local Revenues (k)'] = county_sum_total['Local Revenues'] /1000
+    county_sum_total['Total Revenues (k)'] = county_sum_total['Total Revenues'] /1000
+    county_sum_total = county_sum_total.drop(['Real Property Taxes and Assessments', 
+                                              'Local Revenues','Total Revenues'], axis = 1)
     
-    ## Graphs
+    ## Municipal budget to Municipal summary table
+    town_budget['Municipality Name'] = town_budget['Entity Name'].str.replace('^Town of ', '', regex=True)
+    munic_sum_total = munic_sum_total.merge(town_budget[['Municipality Name','Real Property Taxes and Assessments',
+                                                             'Local Revenues','Total Revenues']],
+                                              on = 'Municipality Name', how = 'left')
+    ### Converting to (k)
+    
+    munic_sum_total['Real Property Taxes and Assessments (k)'] = munic_sum_total['Real Property Taxes and Assessments'] /1000
+    munic_sum_total['Local Revenues (k)'] = munic_sum_total['Local Revenues'] /1000
+    munic_sum_total['Total Revenues (k)'] = munic_sum_total['Total Revenues'] /1000
+    munic_sum_total = munic_sum_total.drop(['Real Property Taxes and Assessments',
+                                                             'Local Revenues','Total Revenues'], axis = 1)
+
+    # Calculating percentage of revenue from select tax
+    ## County
+    county_sum_total['% Property revenue from select tax'] = county_sum_total['sum'] / county_sum_total['Real Property Taxes and Assessments (k)']
+    county_sum_total['% Local revenue from select tax'] = county_sum_total['sum'] / county_sum_total['Local Revenues (k)']
+    county_sum_total['% Total revenue from select tax'] = county_sum_total['sum'] / county_sum_total['Total Revenues (k)']
+
+    ## Town
+    munic_sum_total['% Property revenue from select tax'] = munic_sum_total['sum'] / munic_sum_total['Real Property Taxes and Assessments (k)']
+    munic_sum_total['% Local revenue from select tax'] = munic_sum_total['sum'] / munic_sum_total['Local Revenues (k)']
+    munic_sum_total['% Total revenue from select tax'] = munic_sum_total['sum'] / munic_sum_total['Total Revenues (k)']
+
+    
+   ## Graphs
     plt.rcParams["font.family"] = "Times New Roman"
     plt.rcParams["figure.figsize"] = [7.00, 7.00]
     plt.rcParams["figure.autolayout"] = True
@@ -328,7 +352,7 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     # Renaming sum column in all datasets
     datasets = [county_sum_total, munic_sum_total, school_sum_total, overall_total]
     for dataset in datasets:
-        dataset.rename(columns={'sum': 'sum (k)'}, inplace=True)
+        dataset.rename(columns={'sum': 'Total Select Tax Received (k)'}, inplace=True)
     del [dataset, datasets]
     
     # Merging With Census Data
@@ -339,7 +363,6 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
     munic_sum_total = munic_sum_total.merge(subdiv_census, left_on='Subdiv FIPS', right_on='county subdivision',how = 'left')
     munic_sum_total = munic_sum_total.drop(['NAME','COUNTY','Unnamed: 0',
                                               'GEO_ID','state','county'],axis = 1)
-    
     
     # Cleanup
     func_parcel_tax = func_parcel_tax.drop('_merge',axis = 1)
@@ -355,12 +378,25 @@ def export_tax_data(func_parcel_tax, prefix='', pclass ='' ):
         overall_total.to_excel(writer, sheet_name = 'Overall Summary')
                 
 #%% Functions
-###### Data Export functions
+'''
+    Data Export function (region set, prefix, pclass)
+        Regions: cat_parcel_tax, adk_parcel_tax, all_parcel_tax
+        
+        Prefix: This is the naming convention which will attach to each file output
+        
+        Property Classes:
+            class 931 = tax 532a Taxable Forest Preserve
+            class 980 = Taxable state-owned conservation easements
+            class 940 = Reforested land and other related conservation purposes
+            class 932 = tax 532b Other State-owned land under Section 532-b, c, d, e, f, or g
+            class 990 = Other taxable state land assessments
+'''
 
 export_tax_data(cat_parcel_tax, prefix='cat', pclass =931)
 export_tax_data(adk_parcel_tax, prefix='adk', pclass =931)
-export_tax_data(all_parcel_tax, prefix='all', pclass =931)
+# export_tax_data(all_parcel_tax, prefix='all', pclass =931)
 
 ## adk/cat region, all parcels, all tax codes. 
 #export_tax_data(all_parcel_tax, prefix='all', pclass ='Allclass')
-######
+
+
